509f216bbb4ef6e7685f067e60534a738cf63261
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-04-01 00:50:17 -0300

    feat: friday handler to send a gif when message content contains the fridayTrigger

---
+++ handlers/friday.go
@@ -0,0 +1,58 @@
+package handlers
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"time"
+
+	"io"
+	"net/http"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	fridayTrigger = "sextou"
+	fridayGifUrl  = "https://media.tenor.com/zGlEbV_bTnIAAAAC/kowalski-familia.gif"
+)
+
+func FridayHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if !strings.Contains(m.Content, fridayTrigger) {
+		return
+	}
+
+	message := &discordgo.MessageSend{
+		Files: []*discordgo.File{},
+	}
+
+	switch time.Now().Weekday() {
+		case time.Friday:
+			message.Content = "Sextouu família"
+			message.Files = append(message.Files, processGifUrl(fridayGifUrl))
+		default:
+			message.Content = "Calma lá família ainda não é sexta!"
+	}
+
+	s.ChannelMessageSendComplex(m.ChannelID, message)
+}
+
+func processGifUrl(url string) *discordgo.File {
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Bad request", err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Failed to get GIT", err)
+	}
+
+	gifFile := &discordgo.File{
+		Name:   "sextou-familia.gif",
+		Reader: bytes.NewReader(body),
+	}
+
+	return gifFile
+}
